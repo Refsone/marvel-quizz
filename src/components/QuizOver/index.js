04bb23a5fab90a70cccaa32a307f365085befba1
MANAUT <tristanmanaut@gmail.com>
2020-09-10 15:55:26 +0200

    add modal and localstorage

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { GiTrophyCup } from 'react-icons/gi'
 import Loader from '../Loader'
+import Modal from '../Modal'
+import axios from 'axios'
 
 const QuizOver = React.forwardRef((props, ref) => {
     
@@ -13,14 +15,65 @@ const QuizOver = React.forwardRef((props, ref) => {
         loadLevelQuestions
     } = props
 
+    const API_PUBLIC_KEY = process.env.REACT_APP_MARVEL_API_KEY
+    const hash = '169130f941c7f2964e8cd1f700d6a872'
+    
     const [asked, setAsked] = useState([])
-    console.log(asked)
+    const [openModal, setOpenModal] = useState(false)
+    const [characterInfo, setCharacterInfo] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         setAsked(ref.current)
+
+        if(localStorage.getItem('marvelStorageDate')) {
+            const date = localStorage.getItem('marvelStorageDate')
+            checkDataAge(date)
+        }
     },[ref])
 
+    const checkDataAge = date => {
+        const today = Date.now()
+        const timeDifference = today - date
 
+        const daysDifference = timeDifference / (1000 * 3600 * 24) 
+
+        if (daysDifference >= 15 ) {
+            localStorage.clear();
+            localStorage.setItem('marvelStorageDate', Date.now())
+        }
+    }
+
+    const showModal = id => {
+        setOpenModal(true)
+
+        if(localStorage.getItem(id)) {
+
+            setCharacterInfo(JSON.parse(localStorage.getItem(id)))
+            setLoading(false)
+
+        } else {
+
+            axios
+            .get(`https://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=${API_PUBLIC_KEY}&hash=${hash}`)
+            .then(res => {
+                setCharacterInfo(res.data)
+                setLoading(false)
+                localStorage.setItem(id, JSON.stringify(res.data))
+                if(!localStorage.getItem('marvelStorageDate')) {
+                    localStorage.setItem('marvelStorageDate', Date.now())
+                }
+            })    
+            .catch(error => console.log(error)) 
+        }
+
+     
+    }
+
+    const hideModal = () => {
+        setOpenModal(false)
+        setLoading(true)
+    }
    
     const averageGrade = maxQuestions / 2
 
@@ -86,7 +139,12 @@ const QuizOver = React.forwardRef((props, ref) => {
                     <td>{question.question}</td>
                     <td>{question.answer}</td>
                     <td>
-                        <button className="btnInfo">Info</button>
+                        <button 
+                            className="btnInfo"
+                            onClick={() => showModal(question.heroId)}
+                        >
+                        Info
+                        </button>
                     </td>
                 </tr>
             )
@@ -104,7 +162,30 @@ const QuizOver = React.forwardRef((props, ref) => {
         </tr>
     )
     
-
+    const resultInModal = !loading ? (
+        <>
+        <div className="modalHeader">
+            <h2>{characterInfo.data.results[0].name}</h2>
+        </div>
+        <div className="modalBody">
+            <h3>Titre 2</h3>
+        </div>
+        <div className="modalFooter">
+            <button className="modalBtn">Fermer</button>
+        </div>
+    </>
+    )
+    :
+    (
+        <>
+        <div className="modalHeader">
+            <h2>Réponse de Marvel...</h2>
+        </div>
+        <div className="modalBody">
+            <Loader />
+        </div>
+    </>
+    )
     return (
         <>
            {decision}
@@ -125,6 +206,12 @@ const QuizOver = React.forwardRef((props, ref) => {
                     </tbody>
                 </table>
             </div>
+            <Modal 
+                showModal={openModal}
+                hideModal={hideModal}
+                >
+                { resultInModal }
+            </Modal>
         </>
     )
 })
